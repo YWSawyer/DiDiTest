a5c99f9ad493c89569728d20d842fb338d47defd
YWSawyer <yuan_wei_1@163.com>
2017-12-27 10:01:45 +0800

    no message

--- DiDiTest/ViewController.m
+++ DiDiTest/ViewController.m
@@ -6,9 +6,57 @@
 //  Copyright © 2017 dahuoshi. All rights reserved.
 //
 
+
+
 #import "ViewController.h"
 
-@interface ViewController ()
+@protocol TTaskDelegate <NSObject>
+
+- (void)doneTask;
+
+@end
+
+@interface TTask:NSObject
+
+@property (assign, nonatomic) id<TTaskDelegate> delegate;
+
+@property (strong, nonatomic) NSTimer *timer;
+
+- (void)doneSomething;
+
+@end
+
+@implementation TTask
+
+
+- (void)dealloc {
+    NSLog(@"task dealloc...");
+}
+
+- (void)doneSomething {
+    
+    if (self.delegate && [self.delegate respondsToSelector:@selector(doneTask)]) {
+//        self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(ticktick) userInfo:nil repeats:YES];
+        [self.delegate doneTask];
+        NSLog(@"[self.delegate doneTask]");
+    }
+    
+    [self cleanup];
+}
+
+- (void)ticktick {
+    NSLog(@"ticktick");
+}
+
+- (void)cleanup {
+    
+}
+
+@end
+
+@interface ViewController ()<TTaskDelegate>
+
+@property (strong, nonatomic) TTask *task;
 
 @end
 
@@ -17,6 +65,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.task = [[TTask alloc] init];
+    self.task.delegate = self;
+    
+    [_task doneSomething];
 }
 
 
@@ -25,5 +77,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void)doneTask {
+    self.task = nil;
+    NSLog(@"self.task = nil;");
+}
 
 @end
